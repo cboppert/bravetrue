c9d09d12bf3ba0108a7d21a0733d07fe0a309941
Cody Boppert <cboppert@gmail.com>
2017-03-20 23:54:22 -0400

    Excercise one

---
+++ src/bravetrue/chapter_one.clj
@@ -0,0 +1,10 @@
+(ns bravetrue.chapter-one)
+
+(comment
+  "Use the str function"
+)
+
+(defn use-str
+  "Takes in the argument and says hello to it"
+  [input]
+  (prn (str "Hello " input)))
--- src/bravetrue/core.clj
+++ src/bravetrue/core.clj
@@ -1,7 +1,9 @@
 (ns bravetrue.core
-  (:gen-class))
+  (:gen-class)
+  (:require [bravetrue.chapter-one :as one]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  (one/use-str "Claurice")
   (println "Hello, World!"))
